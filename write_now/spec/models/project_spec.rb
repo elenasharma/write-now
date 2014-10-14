f515bcb5c30d777749813a3159a06d4f13348df5
Elena Sharma <elenasharma3@gmail.com>
2014-10-13 22:21:31 -0700

    Update format of project model spec for consistency re: parentheses.

--- write_now/spec/models/project_spec.rb
+++ write_now/spec/models/project_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe Project do
 	let!(:project) {create :project}
 	context "migration validations" do
-		it {should validate_presence_of(:title)}
-		it {should validate_presence_of(:wordcount_goal)}
-		it {should validate_presence_of(:goal_time_limit)}
-		it {should validate_presence_of(:current_wordcount)}
-		it {should validate_presence_of(:active)}
+		it {should validate_presence_of :title}
+		it {should validate_presence_of :wordcount_goal}
+		it {should validate_presence_of :goal_time_limit}
+		it {should validate_presence_of :current_wordcount}
+		it {should validate_presence_of :active}
 
 		# it {should validate_numericality_of(:wordcount_goal)}
 		# it {should validate_numericality_of(:goal_time_limit)}
