73cf1601dae893ea7962c22b3805e883d9c6023d
Elena Sharma <elenasharma3@gmail.com>
2014-10-13 21:51:33 -0700

    Update user model and migration with validations. * add email field with validation for uniqueness. * validating name, email, and password for presence.

--- write_now/app/models/user.rb
+++ write_now/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	has_many :projects
+	validates :name, :email, :password, presence: true
+	validates :email, uniqueness: true
 end
--- write_now/db/migrate/20141013213639_create_users.rb
+++ write_now/db/migrate/20141013213639_create_users.rb
@@ -1,8 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-    	t.string :name
-    	t.string :password
+    	t.string :name, null: false
+    	t.string :email, null: false, unique: true
+    	t.string :password, null: false
     	t.timestamps
     end
   end
