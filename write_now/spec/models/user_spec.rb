70794379420f40076ed15bf2b47c983cddb67de9
Elena Sharma <elenasharma3@gmail.com>
2014-10-13 22:21:38 -0700

    Create user model tests validating presence of name, email, and password. * also validating association with projects.

--- write_now/spec/models/user_spec.rb
+++ write_now/spec/models/user_spec.rb
@@ -1,5 +1,14 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe User do
-# 	let!(:user) {create :user}
-# end
+describe User do
+	let!(:user) {create :user}
+	context "migration validations" do
+		it {should validate_presence_of :name}
+		it {should validate_presence_of :email}
+		it {should validate_presence_of :password}
+	end
+
+	context "association validations" do
+		it {should have_many :projects}
+	end
+end
